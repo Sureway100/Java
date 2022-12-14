c25fecb80d8f2888c64d001a69e28c06bb42e2b0
david sureway <nwachukwudavid100@gmail.com>
2022-12-14 19:05:27 +0100

    vid1

---
+++ VideoSession_MethodChallenge/HelloWorld.java
@@ -0,0 +1,26 @@
+package VideoSession_MethodChallenge;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        int scoreValue = CalculateHighScorePosition(500);
+        displayHighScorePosition("Tim", scoreValue);
+    }
+    public static void displayHighScorePosition(String name, int position){
+        System.out.println(name + " managed to get into position " + position + " on the high score list");
+    }
+
+    public static int CalculateHighScorePosition(int score){
+        int result = 0;
+        if(score >= 1000){
+            result = 1;
+        } else if (score >= 500 ) {
+            result = 2;
+        } else if (score >= 100 ) {
+            result = 3;
+        }else {
+            result = 4;
+        }
+
+        return result;
+    }
+}
