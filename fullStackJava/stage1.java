553467ce6e3a00b850c17fe0989b91634210a39e
david sureway <nwachukwudavid100@gmail.com>
2023-08-17 07:38:36 +0100

    .

---
+++ fullStackJava/stage1.java
@@ -0,0 +1,13 @@
+package fullStackJava;
+
+public class stage1 {
+    public static void main(String[] args) {
+        int min = 60;
+        int hrs = 24;
+        int sec = 60;
+        System.out.println(min * hrs);
+        System.out.println(min * hrs * sec);
+
+    }
+
+}
